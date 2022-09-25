2bde7f06b5c2fb53e4e144ecac5108255fab2f99
Vovandir <106840463+Vovandir@users.noreply.github.com>
2022-09-25 20:47:11 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,7 +4,7 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery')
 
 const galleryImages = galleryItems
-  .map((item) => `<a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"></a>`)
+  .map((item) => `<a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" title="${item.description}" alt="${item.description}"></a>`)
 	.join("");
   
 gallery.insertAdjacentHTML('afterbegin', galleryImages);
